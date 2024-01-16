f17a5d39bbcb24b0b21e75ea912b18909d196ef3
PKostecki <kostecki.przemyslaw99@gmail.com>
2024-01-16 18:06:43 +0100

    Add product list, product details view

--- enigma_ecommerce/urls.py
+++ enigma_ecommerce/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('product/list/',
+         views.ProductListView.as_view(),
+         name='product-list'),
+    path('product/<int:pk>',
+         views.ProductDetailView.as_view(),
+         name='product-detail'
+         ),
 ]
--- enigma_ecommerce/views.py
+++ enigma_ecommerce/views.py
@@ -1,5 +1,24 @@
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic import DetailView
+from .models import Product
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the Enigma-ecommerce index.")
+
+
+class ProductListView(ListView):
+    model = Product
+    paginate_by = 5
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        if self.request.GET.get('sort_by'):
+            queryset = queryset.order_by(self.request.GET.get('sort_by'))
+        return queryset
+
+
+class ProductDetailView(DetailView):
+    model = Product
