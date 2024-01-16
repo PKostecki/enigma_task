4c68410adc10b884f90b1906669b5695ae04f07b
PKostecki <kostecki.przemyslaw99@gmail.com>
2024-01-16 18:05:36 +0100

    Add product, productcategory and order models, add example data

---
+++ enigma_ecommerce/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to="product_images")
+    image_thumbnail = models.ImageField(upload_to="product_images_thumbnails")
+
+    class Meta:
+        ordering = ('pk', )
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    delivery_address = models.TextField(max_length=255)
+    products = models.ManyToManyField(Product, through='OrderItem')
+    order_date = models.DateField(auto_now_add=True)
+    payment_due_date = models.DateField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Order: {self.pk} for {self.customer.username}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.quantity} for {self.product} in Order: {self.order.pk}"
